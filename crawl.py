cb8520474dfe3488506e58f61f11c6fd4e00bdf2
RichardSchulz <84557532+RichardSchulz52@users.noreply.github.com>
2024-01-01 23:44:14 +0100

    refactored prototype

--- crawl.py
+++ crawl.py
@@ -5,65 +5,104 @@ import urllib.request
 import sqlite3
 import time
 
-options = webdriver.ChromeOptions()
-options.add_argument('--ignore-ssl-errors=yes')
-options.add_argument('--ignore-certificate-errors')
 
-print("Starting")
-dbfile = 'file.db'
-con = sqlite3.connect(dbfile)
+class kleinanzeigen_crawler_autos:
+    def __init__(self, db_file: str = "file.db", remote_web_driver_url: str = 'http://localhost:4444/wd/hub', ) -> None:
+        self.options = webdriver.ChromeOptions()
+        self.options.add_argument('--ignore-ssl-errors=yes')
+        self.options.add_argument('--ignore-certificate-errors')
 
-cur = con.cursor()
-print("create table")
-cur.execute("""CREATE TABLE IF NOT EXISTS raw (
+        self.db_file = db_file
+        self.remote_web_driver_url = remote_web_driver_url
+        self.con = None
+        self.cur = None
+        self.driver = None
+        self.page = 1
+        
+    
+    def startup(self):
+        self.con = sqlite3.connect(self.db_file)
+
+        self.cur = self.con.cursor()
+        self.cur.execute("""CREATE TABLE IF NOT EXISTS raw (
                 url varchar(2000) PRIMARY KEY,
                 text varchar(2000),
                 image blob
-)""")
+        )""")
 
-print("open driver")
-driver = webdriver.Remote(
-command_executor='http://localhost:4444/wd/hub',
-options=options
-)
+        self.driver = webdriver.Remote(
+        command_executor=self.remote_web_driver_url,
+        options=self.options
+        )
+        self.driver.maximize_window()
 
-driver.maximize_window()
+        
 
-print("fetch site")
-driver.get("https://www.kleinanzeigen.de/s-autos/c216")
+    def close(self):
+        if self.driver is not None:
+            self.driver.close()
+            self.driver.quit()
 
-url_exists_in_db = False
-while not url_exists_in_db:
-    print("find elements in page")
-    items_in_page = driver.find_elements(By.CLASS_NAME, 'ad-listitem    ')
-    for item in items_in_page:
-        # get informations
+    def fetch_all(self):
+        finished = False
+        while not finished:
+            print("fetch page " + str(self.page))
+            items_in_page = self.fetch_from_page()
+            finished = self.save_items(items_in_page)
+            self.page += 1
+        print("finished")
+
+    def save_items(self, items_in_page):
+        for item in items_in_page:
+            img_url = self.fetch_img_url(item)
+            print("fetch " + img_url)
+            data = self.data_as_tuple(item, img_url)
+            exists = self.url_already_saved(img_url)
+            if exists:
+                return True
+            self.save_data(data)
+
+            sleep_time = 10
+            print("sleep "+str(sleep_time)+" before continue")
+            time.sleep(sleep_time)
+        finished = False
+        
+            
+
+
+    def fetch_from_page(self) -> bool:
+        self.driver.get(f"https://www.kleinanzeigen.de/s-autos/seite:{self.page}/c216")
+        return self.driver.find_elements(By.CLASS_NAME, 'ad-listitem    ')
+        
+
+    def fetch_img_url(self, item):
         try:
             img_element = item.find_element(By.TAG_NAME, "img")
-            img_url = img_element.get_attribute("src")
+            return img_element.get_attribute("src")
         except NoSuchElementException:
             print("no img tag found. Skipping.")
-            continue
-        print(f"fetching img {img_url}")
+            return None
+        
+    def data_as_tuple(self, item, img_url):
         text = item.text
-        print(f"loading img {img_url}")
-        urllib.request.urlretrieve(img_url, "tmp.png")
-        print("check if item is in db")
-        if cur.execute(f"""SELECT * from raw WHERE url = '{img_url}'""").fetchone():
-            url_exists_in_db = True
-            print(f"ending because {img_url} already exists")
-        else:
-            print("save to db")
-            with open("tmp.png", 'rb') as f:
-                cur.execute("""
+        img_bytes = urllib.request.urlopen(img_url).read()
+        return (img_url, text, img_bytes)
+    
+    def url_already_saved(self, img_url):
+        return self.cur.execute(f"""SELECT * from raw WHERE url = '{img_url}'""").fetchone()
+    
+    def save_data(self, data):
+        self.cur.execute("""
                     INSERT INTO raw (url, text, image)
                     values (?,?,?)    
-                """, (img_url, text, sqlite3.Binary(f.read())))
-            print("sleep for 30")
-            time.sleep(30)
-    print("going to next page")
-    driver.find_element(By.CLASS_NAME, 'pagination-next').click() #ElementClickInterceptedException "is not clickable at point (966, 861)"
-    # auch möglich https://www.kleinanzeigen.de/s-autos/seite:1/c216
-driver.close()
-driver.quit()
-print("Successfully Completed!")
+                """, (data[0], data[1], sqlite3.Binary(data[2])))
+        self.con.commit()
+        
+
+if __name__ == "__main__":
+    c = kleinanzeigen_crawler_autos(remote_web_driver_url="http://192.168.0.2:4444/wd/hub")
+    try:
+        c.startup()
+        c.fetch_all()
+    finally:
+        c.close()
