127dd33ba62f89194256313b870a3cb61554c0ef
RichardSchulz <84557532+RichardSchulz52@users.noreply.github.com>
2024-01-06 20:35:24 +0100

    more robustness

--- crawl.py
+++ crawl.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import StaleElementReferenceException
+from urllib3.exceptions import ReadTimeoutError
 import urllib.request
 import sqlite3
 import time
@@ -10,9 +11,10 @@ import time
 class KleinanzeigenCrawlerAutos:
 
     SHORT_SLEEP = 1
-    NORMAL_SLEEP = 10
+    NORMAL_SLEEP = 5
+    CONNECTION_TIMEOUT_SLEEP = (5 * 60) + 30
 
-    def __init__(self, db_file: str = "kl_cars.db", remote_web_driver_url: str = 'http://localhost:4444/wd/hub', ) -> None:
+    def __init__(self, db_file: str = "kl_cars.db", remote_web_driver_url: str = 'http://localhost:4444/wd/hub') -> None:
         self.options = webdriver.ChromeOptions()
         self.options.add_argument('--ignore-ssl-errors=yes')
         self.options.add_argument('--ignore-certificate-errors')
@@ -23,10 +25,11 @@ class KleinanzeigenCrawlerAutos:
         self.cur = None
         self.driver = None
         self.page = 1
-        self.next_sleep_time = 10
+        self.next_sleep_time = 0
         
     
     def startup(self):
+        self.page = 1
         self.con = sqlite3.connect(self.db_file)
 
         self.cur = self.con.cursor()
@@ -50,14 +53,16 @@ class KleinanzeigenCrawlerAutos:
         if self.driver is not None:
             self.driver.close()
             self.driver.quit()
+        if self.con is not None:
+            self.con.close()
 
 
     def fetch_all(self):
-        finished = False
-        while not finished:
+        self.finished = False
+        while not self.finished:
             print("fetch page " + str(self.page))
             items_in_page = self.fetch_from_page()
-            finished = self.save_items(items_in_page)
+            self.finished = self.save_items(items_in_page)
             self.page += 1
         print("finished")
 
@@ -65,19 +70,34 @@ class KleinanzeigenCrawlerAutos:
     def save_items(self, items_in_page):
         for item in items_in_page:
             data = self.fetch_data(item[0])
-            if data is None:
+            if data is None:  
                 continue
             exists = self.id_already_saved(data[0])
             img_bytes = self.img_bytes(item[1])
             if exists:
+                print(f"id {data[0]} already in db")
                 return True
             self.save_data(data, img_bytes)
         return False
                 
+    def get_and_retry(self, url):
+        retries = 0
+        while retries < 5:
+            try:
+                self.driver.get(url)
+                self.sleep()
+                return
+            except ReadTimeoutError:
+                sleep_time = KleinanzeigenCrawlerAutos.CONNECTION_TIMEOUT_SLEEP
+                print(f"ReadTimeoutError on driver get. Retrying in {sleep_time} seconds.")
+                self.close()
+                time.sleep(sleep_time)
+                self.startup()
+                retries += 1
+
 
     def fetch_from_page(self):
-        self.driver.get(f"https://www.kleinanzeigen.de/s-autos/seite:{self.page}/c216")
-        self.sleep()
+        self.get_and_retry(f"https://www.kleinanzeigen.de/s-autos/seite:{self.page}/c216")
         items = self.driver.find_elements(By.CLASS_NAME, 'ad-listitem    ')
         infos = []
         for item in items:
@@ -105,18 +125,17 @@ class KleinanzeigenCrawlerAutos:
     def fetch_data(self, url):
         try:
             print(f"fetch details from {url}")
-            self.driver.get(url)
-            self.sleep()
+            self.get_and_retry(url)
             self.next_sleep_time = KleinanzeigenCrawlerAutos.NORMAL_SLEEP
-            details = self.driver.find_element(By.ID, "viewad-details").text.split("\n")
+            details = self.driver.find_element(By.ID, "viewad-details").text.split("\n") # FIXME auch hier kann es zu problemen kommen, die zum absturz führen, da auf den remote driver zugegriffen wird 
             brand = details[1]
             model = details[3]
             details_text = ",".join(details[4:])
             cb_details = self.driver.find_element(By.ID, "viewad-configuration").text.replace("\n", ",")
             id = self.driver.find_element(By.ID, "viewad-ad-id-box").text.split("\n")[1]
             return (id, brand, model, details_text, cb_details)
-        except Exception as e:
-            print(e)
+        except NoSuchElementException:
+            print("NoSuchElementException")
 
         
         
@@ -141,6 +160,7 @@ class KleinanzeigenCrawlerAutos:
                     values (?,?,?,?,?,?)    
                 """, (data[0], data[1], data[2], data[3], data[4], sqlite3.Binary(img_bytes)))
         self.con.commit()
+           
 
     def sleep(self):
         print(f"Sleep for {self.next_sleep_time}")
